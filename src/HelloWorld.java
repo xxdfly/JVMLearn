509a94923bd05bc18369d3ec02fc34dfcb3460f8
wb-xxd249566 <wb-xxd249566@alibaba-inc.com>
2017-04-01 11:34:51 +0800

    branch test

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,7 @@
  * Created by wb-xxd249566 on 2017/3/31.
  */
 public class HelloWorld {
+    /*main*/
     public static void main(String[] args){
         System.out.println("Hello world");
     }
